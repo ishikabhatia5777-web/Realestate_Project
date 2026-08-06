e64ebcb8e237ed7935b0144747fe607f5a378c2d
sneha543-gu <snehagupta1786@gmail.com>
2026-08-06 09:19:35 +0530

    Lock recharts version to 2.13.0 and set chunkSizeWarningLimit to 3000

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     }
   },
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 3000,
     rollupOptions: {
       output: {
         manualChunks(id) {
